6c9f9e6a93fd4d193ac69282f0768b7b66271444
João <joaojp0206@gmail.com>
2026-09-02 13:25:48 -0300

    implementa fluxo de consulta de produtos

--- interface.py
+++ interface.py
@@ -38,6 +38,8 @@ def menu(lista, msg):
 def menu_dicio(dicio, msg, msg_opcao):
     titulo(msg)
     print()
+    print('Campos de filtragem:')
+    print()
     for chave, valor in dicio.items():
         print(f'[{chave}] {valor}')
     print()
@@ -45,3 +47,55 @@ def menu_dicio(dicio, msg, msg_opcao):
     escolha_com_espaco = escolha.replace(',', ' ')
     escolha_separada = escolha_com_espaco.split()
     return escolha_separada
+
+
+def detalhes_produto(dicio, codigo):
+    titulo('DETALHES DO PRODUTO')
+    print()
+    print(f'{"Código:":<18}{codigo}')
+    produto = dicio[codigo]
+    for chave, valor in produto.items():
+        print(f'{(chave + ":"):<18}{valor}')
+    print()
+
+    opcoes_tela = [
+        'Alterar cadastro',
+        'Inativar produto',
+        'Voltar',
+        'Menu principal'
+    ]
+
+    c = 1
+    for item in opcoes_tela:
+        print(f'[{c}] {item}')
+        c += 1
+    print()
+    opcao = leiaInt('Sua opção: ')
+    while True:
+        if opcao < 1 or opcao > len(opcoes_tela):
+            print('\nERRO: Por favor, digite apenas opções válidas!\n')
+            opcao = leiaInt('Sua opção: ')
+        else:
+            return opcao
+
+
+def mostrar_resultados(dicio, lista):
+    titulo('PRODUTOS ENCONTRADOS')
+    print()
+    c = 1
+    for codigo in lista:
+        print(f'[{c}] Código {codigo} => ', end=' ')
+        print(f'Tipo: {dicio[codigo]["Tipo"]}', end=' | ') 
+        print(f'Marca: {dicio[codigo]["Marca"]}') 
+        c += 1  
+        print()
+
+    opcao = leiaInt('Digite a númeração do produto para exibir detalhes [valor negativo = CANCELAR]: ')
+    while True:
+        if opcao < 0:
+            return 0
+        elif opcao > len(lista) or opcao == 0:
+            print('\nERRO: Por favor, digite apenas opções válidas!\n')
+            opcao = leiaInt('Digite a númeração do produto para exibir detalhes [valor negativo = CANCELAR]: ')
+        else:
+            return lista[opcao-1]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from time import sleep
-from interface import menu, titulo
+from interface import menu, titulo, detalhes_produto, mostrar_resultados
 from consultas import escolher_filtros, filtrar_produtos
 from dados import estoque, opcoes_filtro
 
@@ -14,15 +14,40 @@ opcoes = [
 
 while True:
     opcao = menu(opcoes, 'SISTEMA DE ESTOQUE')
+    escolha = 0
     if opcao == 1:
         titulo(opcoes[0].upper())
     elif opcao == 2:
-        filtros = escolher_filtros(opcoes_filtro)
-        print(filtros)
-        print(filtrar_produtos(estoque, filtros))
+        while True:
+            if escolha == 4:
+                break
+            else:
+                filtros = escolher_filtros(opcoes_filtro)
+                codigos = filtrar_produtos(estoque, filtros)
+                print()
+                if len(codigos) == 0:
+                    print('Nenhum produto encontrado')
+                elif len(codigos) == 1:
+                    escolha = detalhes_produto(estoque, codigos[0])
+                    if escolha == 3:
+                        continue
+                    
+                else:
+                    while True:
+                        retorno = mostrar_resultados(estoque, codigos)
+                        if retorno == 0:
+                            break
+                        else:
+                            escolha = detalhes_produto(estoque, retorno)
+                            if escolha == 3:
+                                continue
+                            elif escolha == 4:
+                                break
+                            
     elif opcao == 3:
         titulo(opcoes[2].upper())
     elif opcao == 4:
         print('Finalizando o programa...')
         break
     sleep(1)
+    
